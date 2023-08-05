3d6d0c64bf0c1414aeed3cd27a59dfb80885ae01
JoaoMateus98 <joaobsu98@gmail.com>
2023-08-05 18:13:33 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  // for github pages deployment
+  site: "https://JoaoMateus98.github.io",
+  // enter your repo here e.g. "/my-repo"
+  base: "",
+});
